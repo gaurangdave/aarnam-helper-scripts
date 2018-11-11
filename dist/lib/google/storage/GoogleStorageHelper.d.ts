83dd1c3774523687606b77ca07d6d3d2bb57b275
Gaurang Dave <gaurangvdave@gmail.com>
2018-11-11 08:19:13 -0800

    added params type for google storage helper

--- dist/lib/google/storage/GoogleStorageHelper.d.ts
+++ dist/lib/google/storage/GoogleStorageHelper.d.ts
@@ -3,13 +3,30 @@ export declare class GoogleStorageHelper {
     private _storage;
     private _className;
     constructor(keyFilename: string, projectId: string);
-    createBucket(params: any): Promise<ServiceResponse>;
+    createBucket(params: CreateBucketParams): Promise<ServiceResponse>;
     getBucketList(): Promise<ServiceResponse>;
-    removeBucket(params: any): Promise<ServiceResponse>;
-    emptyBucket(params: any): Promise<ServiceResponse>;
-    bucketExists(params: any): Promise<ServiceResponse>;
-    putObject(params: any): Promise<ServiceResponse>;
-    listObjects(params: any): Promise<ServiceResponse>;
-    getObject(params: any): Promise<ServiceResponse>;
-    removeObject(params: any): Promise<ServiceResponse>;
+    removeBucket(params: BucketNameParams): Promise<ServiceResponse>;
+    emptyBucket(params: BucketNameParams): Promise<ServiceResponse>;
+    bucketExists(params: BucketNameParams): Promise<ServiceResponse>;
+    putObject(params: PutObjectParams): Promise<ServiceResponse>;
+    listObjects(params: BucketNameParams): Promise<ServiceResponse>;
+    getObject(params: FileNameParams): Promise<ServiceResponse>;
+    removeObject(params: FileNameParams): Promise<ServiceResponse>;
 }
+export declare type CreateBucketParams = {
+    bucketName: string;
+    isBucketPublic: boolean;
+    areFilesPublic: boolean;
+};
+export declare type BucketNameParams = {
+    bucketName: string;
+};
+export declare type PutObjectParams = {
+    bucketName: string;
+    filePath: string;
+    isPublic: boolean;
+};
+export declare type FileNameParams = {
+    bucketName: string;
+    fileName: string;
+};
