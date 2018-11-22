9967dfb8c5e61584138e535e872ebb99053108e8
Gaurang Dave <gaurangvdave@gmail.com>
2018-11-22 13:16:03 -0800

    added content walker in walker module

--- dist/lib/walker/index.d.ts
+++ dist/lib/walker/index.d.ts
@@ -1,3 +1,4 @@
 import { Observable } from "rxjs";
 export declare const directoryWalker: (dirName: string) => Observable<string>;
 export declare const fileWalker: (dirName: string) => Observable<string>;
+export declare const contentWalker: (dirName: string) => Observable<string>;
